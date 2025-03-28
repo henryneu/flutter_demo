be3877a2c1dbcb803a8e2dad75729fec6030e3a4
zhouzhenhua <zhouzhenhua602747@pwrd.com>
2025-03-28 13:24:12 +0800

    route manager

---
+++ lib/route/route_result_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/**
+ * @Author: zhouzhenhua
+ * @Version: 1.0.0
+ * @Description: 
+ */class RouteResultPage extends StatelessWidget {
+const RouteResultPage({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+return Container();
+}
+}
